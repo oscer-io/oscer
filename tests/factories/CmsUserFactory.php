d2a150f650f80ffa81fbb8c2ab2e8e0c3e117c99
Manuel Christlieb <manuel@christlieb.eu>
2020-03-02 13:18:18 +0100

    wip

--- tests/factories/CmsUserFactory.php
+++ tests/factories/CmsUserFactory.php
@@ -21,6 +21,6 @@ $factory->define(\Bambamboole\LaravelCms\Models\CmsUser::class, function (Faker 
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'bio' => $faker->paragraph,
+        'bio' => $faker->paragraphs(rand(1,3), true),
     ];
 });
