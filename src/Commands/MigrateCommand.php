1d542fdfd9302971db3a4c74f1dbeb3a1948eba0
slashplus <sp@slashplus.de>
2020-03-04 17:49:03 +0100

    refactor(): Change CmsPage naming to Page

--- src/Commands/MigrateCommand.php
+++ src/Commands/MigrateCommand.php
@@ -2,7 +2,7 @@
 
 namespace Bambamboole\LaravelCms\Commands;
 
-use Bambamboole\LaravelCms\Models\CmsPage;
+use Bambamboole\LaravelCms\Models\Page;
 use Bambamboole\LaravelCms\Models\CmsUser;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Date;
@@ -46,7 +46,7 @@ class MigrateCommand extends Command
 
         $shouldCreateNewCmsPage =
             ! Schema::connection(config('cms.database_connection'))->hasTable('cms_pages') ||
-            ! CmsPage::count();
+            ! Page::count();
 
         $this->call('migrate', [
             '--database' => config('cms.database_connection'),
@@ -62,29 +62,6 @@ class MigrateCommand extends Command
                 'bio' => 'This is me.',
                 'email' => $email,
                 'password' => Hash::make($password),
-            ]);
-
-            $this->line('');
-            $this->line('Laravel cms is ready for use. Enjoy!');
-            $this->line('User email: <info>'.$email.'</info>');
-            $this->line('Password: <info>'.$password.'</info>');
-        }
-
-        if ($shouldCreateNewCmsPage) {
-            $user =
-            $email = $this->argument('email') ?? 'admin@admin.com';
-            $password = $this->argument('password') ?? 'password';
-
-            CmsPage::create([
-                'author' => 1,
-                'type' => 'page',
-                'name' => 'first-page',
-                'title' => 'First Page',
-                'content' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ',
-                'excerpt' => 'Lorem ipsum dolor sit amet...',
-                'date' => Date::make(now()),
-                'status' => 'draft',
-                'modified' => Date::make(now()),
             ]);
 
             $this->line('');
