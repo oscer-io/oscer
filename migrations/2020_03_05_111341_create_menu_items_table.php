337dd60dbf94a7f84e79507874e5f5d8d90981eb
Manuel Christlieb <manuel@christlieb.eu>
2020-03-06 20:57:11 +0100

    feat(#10): Add MenuItem model with migration

---
+++ migrations/2020_03_05_111341_create_menu_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('menu');
+            $table->string('url');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
---
+++ src/Models/MenuItem.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Bambamboole\LaravelCms\Models;
+
+
+class MenuItem extends BaseModel
+{
+
+}
