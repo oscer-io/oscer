bdf96ca68cdf34623a844986053fe4257174b10f
Manuel Christlieb <manuel@christlieb.eu>
2020-03-07 17:31:12 +0100

    fix(#10): Remove alpine js stuff

--- resources/js/Layout.vue
+++ resources/js/Layout.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-white">
-        <nav x-data="{ open: false }" class="bg-white border-b border-gray-200">
+        <nav  class="bg-white border-b border-gray-200">
             <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
                     <div class="flex">
