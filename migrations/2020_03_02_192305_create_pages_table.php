e3a383f036c9f4a9b0dbe8d0df60dff523b2d1cc
slashplus <sp@slashplus.de>
2020-03-07 16:28:42 +0100

    feat(gh#15): Add pages migration

---
+++ migrations/2020_03_02_192305_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('author_id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->timestamps();
+            $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
