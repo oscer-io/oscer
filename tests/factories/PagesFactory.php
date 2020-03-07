bae4a250c72809a737fcc47972a720f96b655ce3
slashplus <sp@slashplus.de>
2020-03-07 17:05:54 +0100

    feat(gh#15): Add test pagesfactory

---
+++ tests/factories/PagesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var Factory $factory */
+
+use Bambamboole\LaravelCms\Models\Page;
+use Bambamboole\LaravelCms\Models\User;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Page::class, function (Faker $faker) {
+    return [
+        'author_id' => User::all()->random()->user_id(),
+        'name' => $faker->name,
+        'slug' => $faker->unique()->slug,
+        'body' => $faker->randomLetter,
+    ];
+});
