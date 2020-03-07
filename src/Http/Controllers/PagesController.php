7f1e017d28e5ae36f991972dd626ffb1634a08ce
slashplus <sp@slashplus.de>
2020-03-07 16:50:21 +0100

    feat(gh#15): Add pages controller

--- src/Http/Controllers/PagesController.php
+++ src/Http/Controllers/PagesController.php
@@ -2,12 +2,23 @@
 
 namespace Bambamboole\LaravelCms\Http\Controllers;
 
+use Bambamboole\LaravelCms\Models\Page;
 use Inertia\Inertia;
 
 class PagesController
 {
     public function index()
     {
-        return Inertia::render('Pages/Index');
+        return Inertia::render('Pages/Index', ['pages' => Page::all()]);
+    }
+
+    public function show(Page $page)
+    {
+        return Inertia::render('Pages/Show', ['page' => $page]);
+    }
+
+    public function edit(Page $page)
+    {
+        return Inertia::render('Pages/Edit', ['page' => $page]);
     }
 }
