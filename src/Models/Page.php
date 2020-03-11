a39857975419a9b286bacef9b1cdffc5b2c305ae
slashplus <sp@slashplus.de>
2020-03-11 15:55:50 +0100

    feat(gh#15): Add author accessor, Add body attribute default value

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -2,6 +2,37 @@
 
 namespace Bambamboole\LaravelCms\Models;
 
+
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * @property string avatar
+ */
 class Page extends BaseModel
 {
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'body' => '',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['author'];
+
+    /**
+     * Get the authors avatar or the current authenticated users if the author object is null (i.e. create)
+     *
+     * @return string
+     */
+    public function getAuthorAttribute()
+    {
+        return is_null($this->author_id) ? Auth::user() : User::find($this->author_id);
+    }
 }
