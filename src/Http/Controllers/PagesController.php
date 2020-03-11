7c422ddcb0b923f66311a599a99b3c75bcbdf0c6
slashplus <sp@slashplus.de>
2020-03-11 14:16:48 +0100

    feat(gh#15): Add create and store methods

--- src/Http/Controllers/PagesController.php
+++ src/Http/Controllers/PagesController.php
@@ -2,7 +2,10 @@
 
 namespace Bambamboole\LaravelCms\Http\Controllers;
 
+use Bambamboole\LaravelCms\Http\Requests\CreatePageRequest;
 use Bambamboole\LaravelCms\Models\Page;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class PagesController
@@ -20,5 +23,22 @@ class PagesController
     public function edit(Page $page)
     {
         return Inertia::render('Pages/Edit', ['page' => $page]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Pages/Create', ['page' => new Page()]);
+    }
+
+    public function store(CreatePageRequest $request)
+    {
+        $page = Page::create(array_merge(
+            [
+                'author_id' => Auth::user()->id,
+            ], $request->validated()));
+
+        session()->flash('message', ['type' => 'success', 'text' => "Page {$page->name} created"]);
+
+        return Redirect::route('cms.pages.show', ['page' => $page]);
     }
 }
