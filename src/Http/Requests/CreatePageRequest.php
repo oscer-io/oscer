6740b751f48964fd882b98052dc1f7189ceb2578
slashplus <sp@slashplus.de>
2020-03-11 15:54:02 +0100

    feat(gh#15): Initial commit of CreatePageRequest class

---
+++ src/Http/Requests/CreatePageRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bambamboole\LaravelCms\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreatePageRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $uniqueRule = Rule::unique(config('cms.database_connection') . '.pages', 'slug');
+
+        return [
+            'name' => ['required', 'string'],
+            'slug' => [
+                'required',
+                'string',
+                'regex:/^[a-zA-Z0-9-]+$/', // like alpha_num but with dashes
+                $uniqueRule
+            ],
+            'body' => ['filled', 'string'],
+        ];
+    }
+}
