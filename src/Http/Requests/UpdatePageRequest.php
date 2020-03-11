c9cf6d757f31ba9445676fee32c140d184031f14
slashplus <sp@slashplus.de>
2020-03-11 15:53:41 +0100

    feat(gh#15): Initial commit of UpdatePageRequest class

---
+++ src/Http/Requests/UpdatePageRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bambamboole\LaravelCms\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdatePageRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $uniqueRule = Rule::unique(config('cms.database_connection') . '.pages', 'slug')
+            ->ignore($this->route('page'));
+
+        return [
+            'name' => ['filled', 'string'],
+            'slug' => [
+                'filled',
+                'string',
+                'regex:/^[a-zA-Z0-9-]+$/', // like alpha_num but with dashes
+                $uniqueRule
+            ],
+            'body' => ['filled', 'string'],
+        ];
+    }
+}
