29683a6e5c37bcb79da41f2ddcc9380d1da74d06
slashplus <sp@slashplus.de>
2020-03-12 09:38:02 +0100

    style(#10): Correct indentation

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -12,11 +12,11 @@
                 </div>
                 <div class="mt-4 flex md:mt-0 md:ml-4">
                     <span class="ml-3 shadow-sm rounded-md">
-      <inertia-link :href="route('cms.posts.edit', {post: post.id})"
-                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-        Edit post
-      </inertia-link>
-    </span>
+                      <inertia-link :href="route('cms.posts.edit', {post: post.id})"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                        Edit post
+                      </inertia-link>
+                    </span>
                 </div>
             </div>
             <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
@@ -27,9 +27,9 @@
                     </div>
                 </div>
                 <div class="sm:col-span-6">
-                                <span class="block text-sm font-medium leading-5 text-gray-700">
-                                    Body
-                                </span>
+                    <span class="block text-sm font-medium leading-5 text-gray-700">
+                        Body
+                    </span>
                     <div class="py-3" v-html="post.body">
                     </div>
                 </div>
