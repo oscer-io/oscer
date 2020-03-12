4d378719c79c62bb55e72ac55497b644539f14e9
slashplus <sp@slashplus.de>
2020-03-12 09:39:35 +0100

    style(#10): Correct indentation, Correct heading element (h3->h1)

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -3,17 +3,17 @@
         <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
             <div class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
                 <div class="ml-4 mt-2">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                    <h1 class="text-lg leading-6 font-medium text-gray-900">
                         Posts
-                    </h3>
+                    </h1>
                 </div>
                 <div class="ml-4 mt-2 flex-shrink-0">
-      <span class="inline-flex rounded-md shadow-sm">
-        <a :href="route('cms.posts.create')"
-           class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline">
-          Create new post
-        </a>
-      </span>
+                  <span class="inline-flex rounded-md shadow-sm">
+                    <a :href="route('cms.posts.create')"
+                       class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline">
+                      Create new post
+                    </a>
+                  </span>
                 </div>
             </div>
         </div>
