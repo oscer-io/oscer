5e996d3734677e3f370c49917eda2faf2b86d654
Manuel Christlieb <manuel@christlieb.eu>
2020-03-13 15:10:53 +0100

    feat(#42): Make the template calls dynamically

--- src/Services/PageRenderer.php
+++ src/Services/PageRenderer.php
@@ -3,15 +3,23 @@
 namespace Bambamboole\LaravelCms\Services;
 
 use Bambamboole\LaravelCms\Models\Page;
+use Bambamboole\LaravelCms\Themes\Theme;
 use Illuminate\Http\Request;
 
 class PageRenderer
 {
+    protected Theme $theme;
+
+    public function __construct(Theme $theme)
+    {
+        $this->theme = $theme;
+    }
+
     public function render(Request $request)
     {
         $slug = end(explode('.', $request->route()->getName()));
         $page = Page::query()->where('slug', $slug)->firstOrFail();
 
-        return view('cms::pages.show', ['page' => $page]);
+        return view($this->theme->getPageTemplate(), ['page' => $page]);
     }
 }
