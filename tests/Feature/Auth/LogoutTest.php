ad5e529644012b0f9be13b343297e033c2e17abe
Manuel Christlieb <manuel@christlieb.eu>
2020-03-13 21:23:18 +0100

    feat(): Add LogoutTest

---
+++ tests/Feature/Auth/LogoutTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bambamboole\LaravelCms\Tests\Feature\Auth;
+
+use Bambamboole\LaravelCms\Models\User;
+use Bambamboole\LaravelCms\Tests\TestCase;
+
+class LogoutTest extends TestCase
+{
+    /** @test */
+    public function it_redirects_after_successful_login()
+    {
+        $user = $this->login();
+        $this->assertAuthenticatedAs($user);
+
+        $this->get('/admin/logout');
+
+        $this->assertGuest();
+    }
+}
