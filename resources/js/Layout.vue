bad878d11daca8064d2d3d770b9ccd225c4423a0
slashplus <sp@slashplus.de>
2020-03-14 00:33:11 +0100

    fix(): Fix auth route string

--- resources/js/Layout.vue
+++ resources/js/Layout.vue
@@ -59,9 +59,9 @@
                                         Your
                                         Profile
                                     </inertia-link>
-                                    <a :href="route('cms.backend.auth.logout')"
+                                    <inertia-link :href="route('cms.auth.logout')"
                                        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Sign
-                                        out</a>
+                                        out</inertia-link>
                                 </div>
                             </div>
                         </div>
@@ -110,9 +110,10 @@
                             Your
                             Profile
                         </inertia-link>
-                        <a :href="route('cms.backend.auth.logout')"
-                           class="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out">Sign
-                            out</a>
+                        <inertia-link :href="route('cms.auth.logout')"
+                           class="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out">
+                            Sign
+                            out</inertia-link>
                     </div>
                 </div>
             </div>
