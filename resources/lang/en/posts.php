e0f2e4bd093a2b5209b163450aaa8f0a784045bc
slashplus <sp@slashplus.de>
2020-03-15 21:15:57 +0100

    feat(#38): Change i18n en translation

--- resources/lang/en/posts.php
+++ resources/lang/en/posts.php
@@ -1,10 +1,21 @@
 <?php
 
 return [
-    'menu_label' => 'Posts',
-    'label' => 'Profile',
-    'toast' => [
-        'updated' => 'Post updated',
-        'created' => 'Post created',
-    ],
+    'edit_page_title' => 'Edit post - \'{name}\' (ID: {id})',
+    'create_page_title' => 'Newposts.php post',
+    'show_page_title' => 'Show post - \'{name}\' (ID: {id})',
+    'index_page_title' => 'Posts',
+    'edit_title' => 'Edit post - \'{name}\'',
+    'create_title' => 'New post',
+    'show_title' => 'Show post - \'{name}\'',
+    'index_title' => 'Posts',
+    'name' => 'Name',
+    'slug' => 'Slug',
+    'body' => 'Body',
+    'tags' => 'Tags',
+    'created_at' => 'Created at',
+    'button_save' => 'Save post',
+    'button_cancel' => 'Cancel',
+    'button_create' => 'Create post',
+    'button_edit' => 'Edit post',
 ];
