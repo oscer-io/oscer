0a2e6970e691a10cf2fb7c35913409b0c110e24a
slashplus <sp@slashplus.de>
2020-03-15 12:31:28 +0100

    refactor(): Use LaravelCms class for init & add bootstrap files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,57 +1,23 @@
-import { InertiaApp } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
-import vClickOutside from 'v-click-outside'
-import Router from './lib/Router';
-import route from './mixins/route';
+import LaravelCms from './bootstrap/LaravelCms.js';
 
+// Bootstrap globals
+const mixins = require('./bootstrap/mixins');
+const plugins = require('./bootstrap/plugins');
+
+window.Vue = Vue;
+window.LaravelCms = LaravelCms;
 window.events = new Vue();
-window.flash = function (type, text) {
+window.flash = (type, text) => {
     window.events.$emit('flash', {'type': type, 'text': text});
 };
 
-import languageBundle from '@kirschbaum-development/laravel-translations-loader/php?parameters={{ $1 }}!@kirschbaum-development/laravel-translations-loader';
-import VueI18n from 'vue-i18n';
-Vue.use(VueI18n);
+LaravelCms.app({
+    el: '#app',
 
-const i18n = new VueI18n({
-    locale: window.locale,
-    messages: languageBundle,
+    i18n: plugins.i18n,
+
+    render: h => plugins.renderInertia(h, 'views'),
 });
 
-Vue.use(vClickOutside);
-Vue.use(InertiaApp);
-Vue.mixin(route);
-
-const app = document.getElementById('app');
-
-// Import frequently used (base) components globally & automatically
-let requireComponent = require.context('./components', true, /Base[A-Z]\w+\.(vue|js)$/);
-requireComponent.keys().forEach(function (fileName) {
-    let baseComponentConfig = requireComponent(fileName);
-    let baseComponentName = baseComponentConfig.name || (
-        fileName
-            .split('/')
-            .pop()
-            .replace(/\.\w+$/, '')
-    );
-    Vue.component(
-        baseComponentName,
-        baseComponentConfig.default || baseComponentConfig
-    )
-});
-
-new Vue({
-    i18n,
-    render: h => h(InertiaApp, {
-        props: {
-            initialPage: JSON.parse(app.dataset.page),
-            resolveComponent: name => require(`./views/${name}`).default,
-            transformProps: props => {
-                return {
-                    ...props,
-                    routes: new Router(props.routes),
-                }
-            },
-        },
-    }),
-}).$mount(app);
+LaravelCms.start();
