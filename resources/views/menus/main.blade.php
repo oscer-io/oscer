f48818b920998a00b2bf5f192191462ee397f94c
Manuel Christlieb <manuel@christlieb.eu>
2020-03-15 21:17:55 +0100

    feat(#73): Add super simple menu output

--- resources/views/menus/main.blade.php
+++ resources/views/menus/main.blade.php
@@ -1,1 +1,6 @@
-main menu
+{{$menu->name}} menu
+<ul>
+    @foreach($menu->items as $item)
+        <li><a href="{{$item['url']}}">{{$item['name']}}</a></li>
+    @endforeach
+</ul>
--- src/View/BladeComponents/MenuBladeComponent.php
+++ src/View/BladeComponents/MenuBladeComponent.php
@@ -2,6 +2,7 @@
 
 namespace Bambamboole\LaravelCms\View\BladeComponents;
 
+use Bambamboole\LaravelCms\Models\Menu;
 use Bambamboole\LaravelCms\Themes\Theme;
 use Illuminate\View\Component;
 
@@ -9,16 +10,19 @@ class MenuBladeComponent extends Component
 {
     protected Theme $theme;
 
-    protected string $name;
+    protected Menu $menu;
 
     public function __construct(Theme $theme, string $name)
     {
         $this->theme = $theme;
-        $this->name = $name;
+        $this->menu = Menu::resolve($name);
     }
 
     public function render()
     {
-        return view($this->theme->getMenus()[$this->name]['template']);
+        return view(
+            $this->theme->getMenus()[$this->menu->name]['template'],
+            ['menu' => $this->menu]
+        );
     }
 }
