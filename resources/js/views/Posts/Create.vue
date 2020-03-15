659e9cedef8c220214d8ff0c2b8d71ac6eb9fd63
slashplus <sp@slashplus.de>
2020-03-15 19:22:27 +0100

    feat(#39): Add i18n language strings

--- resources/js/views/Posts/Create.vue
+++ resources/js/views/Posts/Create.vue
@@ -1,10 +1,10 @@
 <template>
-    <layout title="New post">
+    <layout :title="$t('posts.create_page_title')">
         <div>
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex-1 min-w-0">
                     <h1 class="text-lg leading-6 font-medium text-gray-900">
-                        New post
+                        {{ $t('posts.create_title') }}
                     </h1>
                 </div>
             </div>
@@ -12,7 +12,7 @@
                 <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
                     <div class="sm:col-span-6">
                         <label for="name" class="block text-sm font-medium leading-5 text-gray-700">
-                            Name
+                            {{ $t('posts.name') }}
                         </label>
                         <div class="mt-1 rounded-md shadow-sm">
                             <input id="name" type="text" v-model="form.name"
@@ -22,7 +22,7 @@
                     </div>
                     <div class="sm:col-span-6">
                         <label for="slug" class="block text-sm font-medium leading-5 text-gray-700">
-                            Slug
+                            {{ $t('posts.slug') }}
                         </label>
                         <div class="mt-1 rounded-md shadow-sm">
                             <input id="slug" type="text" v-model="form.slug"
@@ -32,7 +32,7 @@
                     </div>
                     <div class="sm:col-span-6">
                         <label for="tags" class="block text-sm font-medium leading-5 text-gray-700">
-                            Tags
+                            {{ $t('posts.tags') }}
                         </label>
                         <TagsField class="mt-1" id="tags" v-model="form.tags"
                                    :available-tags="tags" />
@@ -40,13 +40,12 @@
                     </div>
                     <div class="sm:col-span-6">
                         <label for="body" class="block text-sm font-medium leading-5 text-gray-700">
-                            Body
+                            {{ $t('posts.body') }}
                         </label>
                         <MarkdownField id="body"
                                         class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                                         v-model="form.body"/>
                         <p v-if="$page.errors.body" class="mt-2 text-sm text-red-600">{{ $page.errors.body[0]}}</p>
-                        <p class="mt-2 text-sm text-gray-500">Write crazy stuff.</p>
                     </div>
                 </div>
                 <div class="mt-8 border-t border-gray-200 pt-5">
@@ -54,13 +53,13 @@
                         <span class="inline-flex rounded-md shadow-sm">
                             <InertiaLink :href="route('cms.backend.posts.index')"
                                           class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                                Cancel
+                                {{ $t('posts.button_cancel') }}
                             </InertiaLink>
                         </span>
                         <span class="ml-3 inline-flex rounded-md shadow-sm">
                             <button type="submit"
                                     class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                              Save
+                                {{ $t('posts.button_save') }}
                             </button>
                         </span>
                     </div>
