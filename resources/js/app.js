ffeba2c182e4b771b8b7f1b0d681aee891e8eb91
Manuel Christlieb <manuel@christlieb.eu>
2020-03-15 14:15:39 +0100

    change(#73): Refactor that LaravelCms know how to bootstrap everything.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,11 @@
-import Vue from 'vue'
 import LaravelCms from './bootstrap/LaravelCms.js';
 
-// Bootstrap globals
-require('./bootstrap/mixins');
-require('./bootstrap/components');
-const plugins = require('./bootstrap/plugins');
-
-window.Vue = Vue;
-window.LaravelCms = LaravelCms;
-window.events = new Vue();
-window.flash = (type, text) => {
-    window.events.$emit('flash', {'type': type, 'text': text});
-};
-
-LaravelCms.app({
-    el: '#app',
-
-    i18n: plugins.i18n,
-
-    render: h => plugins.renderInertia(h, 'views'),
-});
-
-LaravelCms.start();
+LaravelCms
+    .loadMixins()
+    .loadBaseComponents()
+    .loadDirectives()
+    .registerFlash()
+    .setContainer('#app')
+    .activateInternationalization()
+    .activateInertia()
+    .start();
