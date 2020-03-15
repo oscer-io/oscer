5bffd6634606628cd26090adb3ed0b19a79ed204
slashplus <sp@slashplus.de>
2020-03-15 19:23:37 +0100

    feat(#39): Add i18n en translation

---
+++ resources/lang/de/posts.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
+    'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    'posts.name' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    'edit_page_title' => 'Edit post - \'{name}\' (ID: {id})',
+    'create_page_title' => 'New post',
+    'show_page_title' => 'Show post - \'{name}\' (ID: {id})',
+    'index_page_title' => 'Posts',
+    'edit_title' => 'Edit post - \'{name}\'',
+    'create_title' => 'New post',
+    'show_title' => 'Show post - \'{name}\'',
+    'index_title' => 'Posts',
+    'name' => 'Name',
+    'slug' => 'Slug',
+    'body' => 'Body',
+    'tags' => 'Tags',
+    'created_at' => 'Created at',
+    'button_save' => 'Save post',
+    'button_cancel' => 'Cancel',
+    'button_create' => 'Create post',
+    'button_edit' => 'Edit post',
+];
