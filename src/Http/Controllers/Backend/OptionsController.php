5b28420a8a71c5dfc23b7eaaeabdc7a672704976
Manuel Christlieb <manuel@christlieb.eu>
2020-03-18 19:31:25 +0100

    feat(#93): Update OptionsController with the Intertia stuff

--- src/Http/Controllers/Backend/OptionsController.php
+++ src/Http/Controllers/Backend/OptionsController.php
@@ -5,6 +5,8 @@ namespace Bambamboole\LaravelCms\Http\Controllers\Backend;
 use Bambamboole\LaravelCms\Http\Requests\CreateOrUpdateOptionRequest;
 use Bambamboole\LaravelCms\Models\Option;
 use Bambamboole\LaravelCms\Services\OptionFieldsResolver;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
 
 class OptionsController
 {
@@ -17,11 +19,18 @@ class OptionsController
 
     public function index()
     {
-        return $this->resolver->getOptionFields();
+        return Inertia::render('Options/Index', ['options' => $this->resolver->getOptionFields()]);
     }
 
     public function store(CreateOrUpdateOptionRequest $request)
     {
-        return Option::query()->updateOrCreate($request->validated());
+        $option = Option::query()->updateOrCreate(
+            ['key' => $request->input('key')],
+            ['value' => $request->input('value')]
+        );
+
+        session()->flash('message', ['type' => 'success', 'text' => "Option {$option->key} updated"]);
+
+        return Redirect::route('cms.backend.options.index');
     }
 }
