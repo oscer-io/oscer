07bb2d77ca3e718128df41fe5de91e7757339eeb
Manuel Christlieb <manuel@christlieb.eu>
2020-03-18 19:29:13 +0100

    feat(#93): Add options nav item

--- resources/js/views/Layout.vue
+++ resources/js/views/Layout.vue
@@ -33,11 +33,18 @@
                                 Menus
                             </InertiaLink>
                             <InertiaLink :href="route('cms.backend.users.index')"
-                                          class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm
+                                         class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm
                             font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300
                             focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150
                             ease-in-out">
                                 Users
+                            </InertiaLink>
+                            <InertiaLink :href="route('cms.backend.options.index')"
+                                         class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm
+                            font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300
+                            focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150
+                            ease-in-out">
+                                Options
                             </InertiaLink>
                         </div>
                     </div>
