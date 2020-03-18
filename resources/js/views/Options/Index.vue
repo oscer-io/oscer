0e66e713dffed0eb00124bfb83fe976ae9be2d42
Manuel Christlieb <manuel@christlieb.eu>
2020-03-18 19:32:49 +0100

    feat(#93): Add option index page with tabs for different sections

---
+++ resources/js/views/Options/Index.vue
@@ -0,0 +1,121 @@
+<template>
+    <layout :title="$t('options.index_page_title')">
+        <h1>{{$t('options.index_page_title')}}</h1>
+        <Tabs :options="{ useUrlFragment: false }">
+            <Tab v-for="(fields,tab) in options" :key="tab" :name="tab">
+
+                <OptionField v-for="option in fields"
+                             :key="option.key"
+                             :option="option"/>
+
+            </Tab>
+
+        </Tabs>
+    </layout>
+</template>
+
+<script>
+    import Tab from "../../components/Tab";
+    import Tabs from "../../components/Tabs";
+    import Layout from '../Layout';
+    import OptionField from "./OptionField";
+
+    export default {
+        props: {
+            options: Object
+        },
+        components: {
+            OptionField,
+            Layout,
+            Tab,
+            Tabs,
+        },
+    }
+</script>
+
+<style>
+    .tabs-component {
+        margin: 4em 0;
+    }
+
+    .tabs-component-tabs {
+        border: solid 1px #ddd;
+        border-radius: 6px;
+        margin-bottom: 5px;
+    }
+
+    @media (min-width: 700px) {
+        .tabs-component-tabs {
+            border: 0;
+            align-items: stretch;
+            display: flex;
+            justify-content: flex-start;
+            margin-bottom: -1px;
+        }
+    }
+
+    .tabs-component-tab {
+        color: #999;
+        font-size: 14px;
+        font-weight: 600;
+        margin-right: 0;
+        list-style: none;
+    }
+
+    .tabs-component-tab:not(:last-child) {
+        border-bottom: dotted 1px #ddd;
+    }
+
+    .tabs-component-tab:hover {
+        color: #666;
+    }
+
+    .tabs-component-tab.is-active {
+        color: #000;
+    }
+
+    .tabs-component-tab.is-disabled * {
+        color: #cdcdcd;
+        cursor: not-allowed !important;
+    }
+
+    @media (min-width: 700px) {
+        .tabs-component-tab {
+            background-color: #fff;
+            border: solid 1px #ddd;
+            border-radius: 3px 3px 0 0;
+            margin-right: .5em;
+            transform: translateY(2px);
+            transition: transform .3s ease;
+        }
+
+        .tabs-component-tab.is-active {
+            border-bottom: solid 1px #fff;
+            z-index: 2;
+            transform: translateY(0);
+        }
+    }
+
+    .tabs-component-tab-a {
+        align-items: center;
+        color: inherit;
+        display: flex;
+        padding: .75em 1em;
+        text-decoration: none;
+    }
+
+    .tabs-component-panels {
+        padding: 4em 0;
+    }
+
+    @media (min-width: 700px) {
+        .tabs-component-panels {
+            border-top-left-radius: 0;
+            background-color: #fff;
+            border: solid 1px #ddd;
+            border-radius: 0 6px 6px 6px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .05);
+            padding: 4em 2em;
+        }
+    }
+</style>
