5d9f3486039d75b198327743c56c9fa9987860ea
Manuel Christlieb <manuel@christlieb.eu>
2020-03-20 20:41:00 +0100

    feat(#108): Add Api\PagesController and PageResource

---
+++ src/Http/Resources/PageResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bambamboole\LaravelCms\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->organization_id,
+            'slug' => $this->company_code,
+            'body' => $this->description,
+            'author_id' => $this->author_id,
+            'published_at' => $this->published_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
