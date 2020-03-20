37430aca3f3916cc1bbb9cd739510ec3f4ed44f8
Manuel Christlieb <manuel.christlieb@googlemail.com>
2020-03-20 18:47:35 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Http/Requests/Auth/SendPasswordResetLinkRequest.php
+++ src/Http/Requests/Auth/SendPasswordResetLinkRequest.php
@@ -3,7 +3,6 @@
 namespace Bambamboole\LaravelCms\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class SendPasswordResetLinkRequest extends FormRequest
 {
--- src/Http/Requests/CreateUserRequest.php
+++ src/Http/Requests/CreateUserRequest.php
@@ -3,7 +3,6 @@
 namespace Bambamboole\LaravelCms\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class CreateUserRequest extends FormRequest
 {
