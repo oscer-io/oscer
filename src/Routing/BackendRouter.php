99170852b351c4d1bf5d5c152376aef25789e3ee
Manuel Christlieb <manuel.christlieb@googlemail.com>
2020-03-27 19:20:39 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Routing/BackendRouter.php
+++ src/Routing/BackendRouter.php
@@ -8,12 +8,6 @@ use Bambamboole\LaravelCms\Auth\Http\Middleware\Authenticate;
 use Bambamboole\LaravelCms\Core\Http\Controllers\BackendController;
 use Bambamboole\LaravelCms\Core\Http\Middleware\SetInertiaConfiguration;
 use Bambamboole\LaravelCms\Core\Http\Middleware\SetLocale;
-use Bambamboole\LaravelCms\Menus\Http\Controllers\MenusController;
-use Bambamboole\LaravelCms\Options\Http\Controllers\OptionsController;
-use Bambamboole\LaravelCms\Publishing\Http\Controllers\PagesController;
-use Bambamboole\LaravelCms\Publishing\Http\Controllers\PostsController;
-use Bambamboole\LaravelCms\Users\Http\Controllers\ProfileController;
-use Bambamboole\LaravelCms\Users\Http\Controllers\UsersController;
 use Illuminate\Config\Repository;
 use Illuminate\Routing\Router;
 
