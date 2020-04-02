6bca47956276995d8c2099319ee1386ad899dd95
jchristlieb <mail@janchristlieb.de>
2020-04-02 16:54:42 +0200

    add controller with security layer to pw reset form

---
+++ src/Auth/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Bambamboole\LaravelCms\Auth\Http\Controllers;
+
+use Bambamboole\LaravelCms\Auth\Http\Requests\ResetPasswordRequest;
+use Bambamboole\LaravelCms\Auth\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Throwable;
+
+class ResetPasswordController
+{
+
+    public function showResetForm(Request $request, $encryptedToken)
+    {
+        /** @var \Bambamboole\LaravelCms\Auth\Models\User $user */
+        $user = $this->getUserFromEncryptedToken($encryptedToken);
+
+        return view('cms::auth.reset-password', compact('encryptedToken','user'));
+    }
+
+    public function update(ResetPasswordRequest $request)
+    {
+        /** @var \Bambamboole\LaravelCms\Auth\Models\User $user */
+        $user = $this->getUserFromEncryptedToken($request->input('encrypted_token'));
+        $user->update(['password' => $request->input('password')]);
+
+        Auth::guard()->login($user);
+
+        return redirect()->route('cms.backend.start');
+    }
+
+    protected function getUserFromEncryptedToken($encryptedToken)
+    {
+        try {
+            $token = decrypt($encryptedToken);
+            [$userId, $token] = explode('|', $token);
+            /** @var \Bambamboole\LaravelCms\Auth\Models\User $user */
+            $user = User::query()->findOrFail($userId);
+
+        } catch (Throwable $exception) {
+            return redirect()->route('cms.password.forgot')->with('invalidResetToken', true);
+        }
+        if (cache("password.reset.{$userId}") != $token) {
+            return redirect()->route('cms.password.forgot')->with('invalidResetToken', true);
+        }
+
+        return $user;
+    }
+
+}
