73fdc466847a4d5d73726c649a0a289d23cbea32
jchristlieb <mail@janchristlieb.de>
2020-04-02 15:20:45 +0200

    provide user an input field for own pw

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -22,17 +22,38 @@
                 </div>
             @endif
 
-                    <span class="bg-gray-700 rounded text-sm py-2 px-6 text-white">{{$password}}</span>
-                </div>
-                <p>{{__('cms::auth.pages.new_password.text')}}</p>
-                <div class="mt-10">
-                    <a href="{{route('cms.auth.login')}}"
-                       class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                        {{__('cms::auth.pages.new_password.goto')}}
+            <form class="mt-4" method="POST" action="{{route('cms.password.update')}}">
+                @csrf
+                <input type="hidden" name="encrypted_token" value="{{$encryptedToken}}"/>
+                <div class="">
+                    <div class="my-2">
+                        <input aria-label="Password" name="password" type="password" required
+                               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                               placeholder="Password"/>
+                    </div>
 
-                    </a>
+                    <div class="my-2">
+                        <input aria-label="Password" name="password_confirmation" type="password" required
+                               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                               placeholder="Confirm Password"/>
+                    </div>
                 </div>
-            </div>
+
+                <div class="mt-6">
+                    <button type="submit"
+                            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+          <span class="absolute left-0 inset-y pl-3">
+            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                 fill="currentColor" viewBox="0 0 20 20">
+              <path fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"/>
+            </svg>
+          </span>
+                        {{__('cms::auth.pages.reset_password.reset')}}
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
