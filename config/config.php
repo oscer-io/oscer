01122e05aa98c37d8aec64595010d6c32029e85a
Manuel Christlieb <manuel@christlieb.eu>
2020-04-04 17:26:02 +0200

    feat(): Add resource mapping to config.php

--- config/config.php
+++ config/config.php
@@ -3,12 +3,20 @@
 /*
  * You can place your custom package configuration in here.
  */
+
+use Bambamboole\LaravelCms\Auth\Models\User;
+use Bambamboole\LaravelCms\Menus\Models\MenuItem;
+
 return [
     'from_email' => env('CMS_FROM_EMAIL', 'cms@website.com'),
     'backend' => [
         'domain' => env('CMS_BACKEND_DOMAIN'),
         'url' => 'admin',
         'middleware' => 'web',
+    ],
+    'resources' => [
+      'user' => User::class,
+      'menu-item' => MenuItem::class,
     ],
     'options' => [
         'pages' => [
