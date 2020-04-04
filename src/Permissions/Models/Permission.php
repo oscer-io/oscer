1b8fe7ebfd096f3f1465237b10259523ee29afcd
slashplus <sp@slashplus.de>
2020-04-04 23:33:12 +0200

    feat(gl#70): Add group method

--- src/Permissions/Models/Permission.php
+++ src/Permissions/Models/Permission.php
@@ -4,4 +4,8 @@ namespace Bambamboole\LaravelCms\Permissions\Models;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
+
+    function getGroup(){
+        return strtok($this->name,  '.');
+    }
 }
