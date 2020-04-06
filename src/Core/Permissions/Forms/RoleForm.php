8fe79877fd50bee02d2ee4aa375fc9a2853838ea
slashplus <sp@slashplus.de>
2020-04-06 13:38:19 +0200

    feat(gl#70): Allow updating permissions

--- src/Core/Permissions/Forms/RoleForm.php
+++ src/Core/Permissions/Forms/RoleForm.php
@@ -5,21 +5,41 @@ namespace Bambamboole\LaravelCms\Core\Permissions\Forms;
 use Bambamboole\LaravelCms\Backend\Form\Fields\CheckboxField;
 use Bambamboole\LaravelCms\Backend\Form\Form;
 use Bambamboole\LaravelCms\Core\Permissions\Models\Permission;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 class RoleForm extends Form
 {
+
+    /**
+     * @var Model | \Bambamboole\LaravelCms\Core\Permissions\Models\Role
+     */
+    protected Model $resource;
+
     public function fields(): Collection
     {
-        $role = $this->resource;
+        return Permission::all()->map(function ($permission) {
+            return CheckboxField::make($permission->name, $permission->name)
+                ->rules(['boolean'])
+                ->addResolveValueCallback(function ($value) use ($permission) {
 
-        return Permission::all()->map(function ($permission) use ($role) {
-            $value = $role->hasPermissionTo($permission);
-            $field = CheckboxField::make($permission->name, $permission->name, true)->rules(['required', 'boolean']);
-            $field->addResolveValueCallback(function ($field) use ($value) {
-                return $value;//$rolePermissions->contains($permission);
-            });
-            return $field;
+                    return $this->resource->hasPermissionTo($permission);
+                });
         });
+    }
+
+    protected function updateResource($data)
+    {
+        $this->resource->updatePermissions(collect($data)
+            ->filter() // remove all items with value == false
+            ->keys() // remove all boolean values, just use permission strings
+            ->transform(function ($item) { // transform all incorrect permission strings
+                //todo: remove later. perhaps must be changed in general form implementation
+                $item = str_replace('__asterisk__', '*', $item);
+                $item = str_replace('->', '.', $item);
+
+                return $item;
+            })
+        );
     }
 }
--- src/Core/Permissions/Models/Role.php
+++ src/Core/Permissions/Models/Role.php
@@ -5,14 +5,19 @@ namespace Bambamboole\LaravelCms\Core\Permissions\Models;
 use Bambamboole\LaravelCms\Api\Contracts\HasApiEndpoints;
 use Bambamboole\LaravelCms\Api\Contracts\HasIndexEndpoint;
 use Bambamboole\LaravelCms\Api\Contracts\HasShowEndpoint;
+use Bambamboole\LaravelCms\Api\Contracts\HasStoreEndpoint;
 use Bambamboole\LaravelCms\Backend\Contracts\HasForm;
 use Bambamboole\LaravelCms\Core\Permissions\Forms\RoleForm;
 use Bambamboole\LaravelCms\Core\Permissions\Resources\RoleResource;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
 
 class Role extends \Spatie\Permission\Models\Role implements
     HasForm,
     HasApiEndpoints,
     HasIndexEndpoint,
+    HasStoreEndpoint,
     HasShowEndpoint
 {
     const SUPER_ADMIN_ROLE = 'super-admin';
@@ -27,8 +32,31 @@ class Role extends \Spatie\Permission\Models\Role implements
         return new RoleResource(self::query()->findOrFail($identifier));
     }
 
+    public function executeStore(Request $request)
+    {
+        $form = $this->getForm();
+        $form->setData($request->all());
+        $validator = $form->getValidator();
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        $model = $form->save();
+
+        return $this->asResource($model);
+    }
+
     public function getForm()
     {
         return new RoleForm($this);
+    }
+
+    /**
+     * @param $permissions
+     */
+    public function updatePermissions($permissions)
+    {
+        $this->syncPermissions($permissions);
     }
 }
