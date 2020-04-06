b6d08b0084669c799ad8df6e8e63f45eaac4c095
slashplus <sp@slashplus.de>
2020-04-06 14:34:30 +0200

    feat(gl#70): Allow different submitButton positions

--- resources/js/components/ResourceForm.vue
+++ resources/js/components/ResourceForm.vue
@@ -1,6 +1,22 @@
 <template>
     <loading :loading="isLoading">
         <form @submit.prevent="submitResourceForm">
+            <div v-if="inSubmitPositions('top')" class="mt-8 border-t border-gray-200 pt-5">
+                <div class="flex justify-end">
+                    <span class="inline-flex rounded-md shadow-sm">
+                        <button type="button" @click="$emit('cancel')"
+                                class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                            {{this.cancelText}}
+                        </button>
+                    </span>
+                    <span class="ml-3 inline-flex rounded-md shadow-sm">
+                        <button type="submit"
+                                class="btn">
+                            {{this.submitText}}
+                        </button>
+                    </span>
+                </div>
+            </div>
             <component
                 v-for="(field, index) in fields"
                 :key="field.name + index"
@@ -9,20 +25,20 @@
                 :field="field"
                 :validation-errors="getValidationErrors(field)"
             />
-            <div class="mt-8 border-t border-gray-200 pt-5">
+            <div v-if="inSubmitPositions('bottom')" class="mt-8 border-t border-gray-200 pt-5">
                 <div class="flex justify-end">
-                        <span class="inline-flex rounded-md shadow-sm">
-                            <button type="button" @click="$emit('cancel')"
-                                    class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                                {{this.cancelText}}
-                            </button>
-                        </span>
+                    <span class="inline-flex rounded-md shadow-sm">
+                        <button type="button" @click="$emit('cancel')"
+                                class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                            {{this.cancelText}}
+                        </button>
+                    </span>
                     <span class="ml-3 inline-flex rounded-md shadow-sm">
-                            <button type="submit"
-                                    class="btn">
-                                {{this.submitText}}
-                            </button>
-                        </span>
+                        <button type="submit"
+                                class="btn">
+                            {{this.submitText}}
+                        </button>
+                    </span>
                 </div>
             </div>
         </form>
@@ -31,6 +47,7 @@
 <script>
     import api from "../lib/api";
     import Form from "../lib/mixins/Form";
+    import {difference} from "lodash";
 
     export default {
         mixins: [Form],
@@ -55,6 +72,17 @@
                 type: String,
                 default: 'Submit'
             },
+            submitPositions: {
+                type: Array,
+                validator: function (value) {
+                    const valid = ['top', 'bottom'];
+
+                    return value === undefined || difference(value, valid).length === 0;
+                },
+                default: function () {
+                    return ['bottom']
+                }
+            }
         },
         data() {
             return {
@@ -72,6 +100,12 @@
             this.fetchResourceForm();
         },
         methods: {
+            inSubmitPositions(positions) {
+                if (!Array.isArray(positions)) {
+                    positions = [positions];
+                }
+                return difference(positions, this.submitPositions).length === 0;
+            },
             prepareParams() {
                 // Filter null values. This way we can handle create and update
                 return [this.resource, this.resourceId].filter(el => el !== null)
