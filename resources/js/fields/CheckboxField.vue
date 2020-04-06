2d7eb892f1b5f4c265cb4415437335d0a0c7f825
slashplus <sp@slashplus.de>
2020-04-06 23:08:19 +0200

    feat(gl#70): Remove inputType computed

--- resources/js/fields/CheckboxField.vue
+++ resources/js/fields/CheckboxField.vue
@@ -1,12 +1,12 @@
 <template>
-    <field-wrapper  :name="field.name" :label="field.label || field.name" :errors="validationErrors">
+    <field-wrapper :name="field.name" :label="field.label || field.name" :errors="validationErrors">
         <input
             class="form-checkbox transition duration-150 ease-in-out sm:text-sm sm:leading-5"
             :class="{'border-red-600': !!hasValidationErrors}"
             :id="field.name"
             :name="field.name"
             v-model="value"
-            :type="inputType"
+            type="checkbox"
         />
     </field-wrapper>
 </template>
@@ -14,13 +14,6 @@
     import FormField from "../lib/mixins/FormField";
 
     export default {
-        mixins: [FormField],
-        computed: {
-            inputType() {
-                return this.field.attributes && this.field.attributes.type
-                    ? this.field.attributes.type
-                    : 'checkbox';
-            }
-        }
+        mixins: [FormField]
     }
 </script>
