98e9a120d0bcb02d22445cd2e46c31ee82570b56
slashplus <sp@slashplus.de>
2020-04-06 13:29:30 +0200

    feat(gl#70): Remove unused interface implementations

--- src/Core/Permissions/Models/Permission.php
+++ src/Core/Permissions/Models/Permission.php
@@ -2,28 +2,11 @@
 
 namespace Bambamboole\LaravelCms\Core\Permissions\Models;
 
-use Bambamboole\LaravelCms\Api\Contracts\HasApiEndpoints;
-use Bambamboole\LaravelCms\Api\Contracts\HasIndexEndpoint;
-use Bambamboole\LaravelCms\Api\Contracts\HasShowEndpoint;
-use Bambamboole\LaravelCms\Core\Permissions\Resources\PermissionResource;
-
-class Permission extends \Spatie\Permission\Models\Permission implements
-    HasApiEndpoints,
-    HasIndexEndpoint,
-    HasShowEndpoint
+class Permission extends \Spatie\Permission\Models\Permission
 {
     public function getGroup()
     {
         return strtok($this->name, '.');
     }
 
-    public function executeIndex()
-    {
-        return PermissionResource::collection(self::all());
-    }
-
-    public function executeShow($identifier)
-    {
-        // TODO: Implement executeShow() method.
-    }
 }
