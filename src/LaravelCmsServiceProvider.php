55bc6e65835f0689020c21d31fedc40f6d6e0633
Manuel Christlieb <manuel@christlieb.eu>
2020-04-10 03:08:11 +0200

    clean up ServiceProvider

--- src/LaravelCmsServiceProvider.php
+++ src/LaravelCmsServiceProvider.php
@@ -23,6 +23,12 @@ use Illuminate\View\Factory;
 
 class LaravelCmsServiceProvider extends ServiceProvider
 {
+    protected ApiRouter $apiRouter;
+
+    protected BackendRouter $backendRouter;
+
+    protected Repository $config;
+
     public function boot(
         ApiRouter $apiRouter,
         BackendRouter $backendRouter,
@@ -30,17 +36,23 @@ class LaravelCmsServiceProvider extends ServiceProvider
         Factory $view,
         Repository $config,
         Theme $theme
-    ) {
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'cms');
-        $this->loadMigrationsFrom(__DIR__.'/../migrations');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'cms');
-        $this->registerGuard($config);
+    )
+    {
+        $this->apiRouter = $apiRouter;
+        $this->backendRouter = $backendRouter;
+        $this->config = $config;
+
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'cms');
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'cms');
+
+        $this->configureGuard();
+        $this->configurePermissions();
+        $this->configureSanctum();
+        $this->configureZiggy();
+
         $this->registerPublishes();
-
-        $apiRouter->registerApiRoutes();
-
-        $backendRouter->registerAuthRoutes();
-        $backendRouter->registerBackendRoutes();
+        $this->registerRoutes();
 
         Gate::before(function ($user, $ability) {
             return $user->hasRole(Role::SUPER_ADMIN_ROLE) ? true : null;
@@ -56,45 +68,62 @@ class LaravelCmsServiceProvider extends ServiceProvider
         $blade->component(MenuBladeComponent::class, 'menu');
     }
 
-    /**
-     * Register the package's guard.
-     */
-    protected function registerGuard(Repository $config): void
+    protected function configureGuard(): void
     {
-        $config->set('permission.models', [
-            'permission' => Permission::class,
-            'role'       => Role::class,
-        ]);
-
-        $config->set('auth.providers.cms_users', [
+        $this->config->set('auth.providers.cms_users', [
             'driver' => 'eloquent',
             'model' => User::class,
         ]);
 
-        $config->set('auth.guards.web', [
+        $this->config->set('auth.guards.web', [
             'driver' => 'session',
             'provider' => 'cms_users',
         ]);
-        $statefulHosts = $config->get('sanctum.stateful');
-        $statefulHosts[] = $config->get('cms.backend.domain');
 
-        $config->set('ziggy.whitelist', ['cms.*']);
-        $config->set('ziggy.skip-route-function', true);
-        $config->set('sanctum.stateful', $statefulHosts);
-        $config->set('sanctum.middleware.verify_csrf_token', VerifyCsrfToken::class);
+    }
+
+    protected function configurePermissions()
+    {
+        $this->config->set('permission.models', [
+            'permission' => Permission::class,
+            'role' => Role::class,
+        ]);
+    }
+
+    protected function configureZiggy()
+    {
+        $this->config->set('ziggy.whitelist', ['cms.*']);
+        $this->config->set('ziggy.skip-route-function', true);
+    }
+
+    protected function configureSanctum()
+    {
+        $statefulHosts = $this->config->get('sanctum.stateful');
+        $statefulHosts[] = $this->config->get('cms.backend.domain');
+
+
+        $this->config->set('sanctum.stateful', $statefulHosts);
+        $this->config->set('sanctum.middleware.verify_csrf_token', VerifyCsrfToken::class);
     }
 
     protected function registerPublishes(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../dist' => public_path('vendor/cms'),
+                __DIR__ . '/../dist' => public_path('vendor/cms'),
             ], 'cms-assets');
 
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('cms.php'),
+                __DIR__ . '/../config/config.php' => config_path('cms.php'),
             ], 'cms-config');
         }
+    }
+
+    protected function registerRoutes()
+    {
+        $this->apiRouter->registerApiRoutes();
+        $this->backendRouter->registerAuthRoutes();
+        $this->backendRouter->registerBackendRoutes();
     }
 
     /**
@@ -102,7 +131,7 @@ class LaravelCmsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'cms');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'cms');
 
         $this->commands([
             PublishCommand::class,
