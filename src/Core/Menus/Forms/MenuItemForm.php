6a1d91b5f4e6beaef4235345bfde06b631a27557
slashplus <sp@slashplus.de>
2020-04-11 16:48:38 +0200

    feat(WIP#67): Add fields to choose from an existing post or a custom url

--- src/Core/Menus/Forms/MenuItemForm.php
+++ src/Core/Menus/Forms/MenuItemForm.php
@@ -2,8 +2,12 @@
 
 namespace Bambamboole\LaravelCms\Core\Menus\Forms;
 
+use Bambamboole\LaravelCms\Backend\Form\Fields\SelectField;
+use Bambamboole\LaravelCms\Backend\Form\Fields\SelectListField;
 use Bambamboole\LaravelCms\Backend\Form\Fields\TextField;
 use Bambamboole\LaravelCms\Backend\Form\Form;
+use Bambamboole\LaravelCms\Core\Menus\Models\MenuItem;
+use Bambamboole\LaravelCms\Core\Posts\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
@@ -12,8 +16,70 @@ class MenuItemForm extends Form
     public function fields(): Collection
     {
         return collect([
+            // Name field
             TextField::make('name')->rules(['required', 'string']),
-            TextField::make('url')->rules(['required', 'string']),
+
+            // Type field (url|post-selection)
+            SelectField::make(
+                'type',
+                'Type',
+                function () {
+                    return [
+
+                    ];
+                },
+                function () {
+                    return null;
+                }
+            )
+                ->rules(['required'])
+                ->options( // we pass the names of the fields here
+                    [
+                        [
+                            'name' => 'url',
+                            'label' => 'Custom Url',
+                            'value' => 'url',
+                        ],
+                        [
+                            'name' => 'post',
+                            'label' => 'Posts',
+                            'value' => 'post',
+                        ]
+                    ]
+                )
+            ,
+            TextField::make('url')
+                ->rules(['required', 'string'])
+                ->activate(false)
+                ->dependsOn('type', 'url'),
+
+            // Post selection field
+            SelectListField::make(
+                'post',
+                'Posts',
+                null,
+                function (MenuItem $resource, Request $request) {
+//                    $id = $request->input('post');
+//                    $post = Post::query()->where('slug', $id)->first();
+//                    $resource->url = implode('/', [$post->type, $post->slug]);
+                }
+                )
+                ->rules(['required', 'string'])
+                ->options(
+                    Post::all()
+                        ->map(
+                            function ($post) {
+                                return [
+                                    'name' => $post->name,
+                                    'label' => $post->name,
+                                    'value' => $post->slug,
+                                ];
+                            }
+                        )
+                        ->toArray()
+                )
+                ->activate(false)
+                ->dependsOn('type', 'post')
         ]);
     }
 
