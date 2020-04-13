a0c3135a513504649a98e08de2cb20b7955908a6
slashplus <sp@slashplus.de>
2020-04-13 18:39:22 +0200

    feat(#67): Add phpdocs, Add functionality to set options with consistent data

--- src/Backend/Form/Fields/SelectField.php
+++ src/Backend/Form/Fields/SelectField.php
@@ -2,18 +2,51 @@
 
 namespace Bambamboole\LaravelCms\Backend\Form\Fields;
 
+
 class SelectField extends Field
 {
-    public string $component = 'select-field';
+    public string $component = 'SelectField';
 
     public array $options = [];
 
     protected array $with = ['options'];
 
+    /**
+     * Each option must have a 'name', a 'label' and a 'value' like this
+     * ['name' => 'foo', 'label' => 'bar', 'value' => 'baz'],
+     */
     public function options(array $options)
     {
-        $this->options = $options;
+        $this->options = $this->filterOptions($options);
 
         return $this;
+    }
+
+    /**
+     * Filter an array of options to provide consistent data. Therefore
+     * remove data that does not provide at least a 'name' and
+     * fill 'label' & 'value' with 'name' if they aren't
+     * set neither.
+     */
+    protected function filterOptions(array $options)
+    {
+        $data = [];
+        foreach ($options as $option) {
+            if ($name = $option['name'] ?? '') {
+                $data[] = [
+                    'name' => $name,
+                    'label' => $option['label'] ?? $name,
+                    'value' => $option['value'] ?? $name
+                ];
+            }
+        }
+
+        return $data;
+    }
+
+
+    public function beforeSave()
+    {
+
     }
 }
